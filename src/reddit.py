23c1b1d09493f3b08e854d0ca4e2266cbdc1e7d8
Mr.PowerScripts <PlayingWithPowershell@gmail.com>
2019-11-04 20:37:19 +0000

    I think reddit caught on. oh well.

--- src/reddit.py
+++ src/reddit.py
@@ -303,15 +303,15 @@ def random_reply():
 
 
     try:
-        if prob(.1): # small chance we advertise
-          content = share()
-          comment = random.choice(submission.comments.list())
-          log.info('sharing - thanks for helping out!')
-          sharing = '{} {}'.format(content['comment'], content['url'])
-          reply = comment.reply(sharing)
-          log.info("Replied to comment: {}".format(comment.body))
-          log.info("Replied with: {}".format(reply))
-          return
+#         if prob(.1): # small chance we advertise
+#           content = share()
+#           comment = random.choice(submission.comments.list())
+#           log.info('sharing - thanks for helping out!')
+#           sharing = '{} {}'.format(content['comment'], content['url'])
+#           reply = comment.reply(sharing)
+#           log.info("Replied to comment: {}".format(comment.body))
+#           log.info("Replied with: {}".format(reply))
+#           return
         if prob(.35):  # There's a larger chance that we'll reply to a comment.
             log.info("replying to a comment")
             comment = random.choice(submission.comments.list())
