5dcf5af5a72b79e5410dee9d0a3d1ab672a51c6d
Anonymous4547 <58414668+Anonymous4547@users.noreply.github.com>
2019-12-18 00:37:26 -0800

    Updates utils.py
    Line 27 changed 365d to 365 because it won’t start.

--- src/utils.py
+++ src/utils.py
@@ -24,7 +24,7 @@ DAY = 86400  # POSIX day (exact value)
 MINUTE = 60
 PROBABILITIES = {"REPLY": 0.02, "SUBMISSION": 0.02, "LEARN": 0.02, "DELETE": 0.02}
 MAX_CACHE_SIZE = 128
-NUMBER_DAYS_FOR_POST_TOBE_OLD = 365d
+NUMBER_DAYS_FOR_POST_TOBE_OLD = 365
 SUBREDDIT_LIST = [] # limit learning and posting to these subreddits. Empty = Random
 
 if os.environ.get("SUBREDDIT_LIST"): # Prefer subreddit list from envars
