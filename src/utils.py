6105e566409efb9d2f24e40b9da9c07066cfa262
MrPowerScripts <playingwithpowershell@gmail.com>
2020-01-01 14:38:44 +0000

    older scores now show as 1 on pushshift so this change fixes reposting

--- src/utils.py
+++ src/utils.py
@@ -18,11 +18,11 @@ MAIN_DB_MIN_SIZE = 52428800  # in bytes
 MAIN_DB_MAX_SIZE = 209715200  # in bytes
 SCORE_THRESHOLD = -2  # downvote
 TOP_SUBREDDIT_NUM = 10  # number of subreddits to search for repost-able content
-MIN_SCORE = 30000  # for posts to repost
+MIN_SCORE = 0  # for posts to repost
 SUBMISSION_SEARCH_TEMPLATE = "https://api.pushshift.io/reddit/search/submission/?after={after}&before={before}&sort_type=score&sort=desc&subreddit={subreddit}"
 DAY = 86400  # POSIX day (exact value)
 MINUTE = 60
-PROBABILITIES = {"REPLY": 0.02, "SUBMISSION": 0.02, "LEARN": 0.02, "DELETE": 0.02}
+PROBABILITIES = {"REPLY": 0.02, "SUBMISSION": 0.01, "LEARN": 0.02, "DELETE": 0.02}
 MAX_CACHE_SIZE = 128
 NUMBER_DAYS_FOR_POST_TOBE_OLD = 365
 SUBREDDIT_LIST = [] # limit learning and posting to these subreddits. Empty = Random
