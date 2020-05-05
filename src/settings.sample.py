3c7d7e955af41623d519d12bd1ba6e31c6e585c0
zegevlier <iamascratcher@gmail.com>
2020-05-05 15:22:12 +0200

    Made some small changes.
    I made it more obvious that the user agent can be anything and I changed the order of username and password to have it make more sense (imo).

--- src/settings.sample.py
+++ src/settings.sample.py
@@ -1,6 +1,6 @@
 # Reddit API
 REDDIT_CLIENT_ID="MyCoolClientId"
 REDDIT_SECRET="MyCoolSecret"
-REDDIT_PASSWORD="MyCoolPassword"
 REDDIT_USERNAME="MyCoolUsername"
-REDDIT_USER_AGENT="script by /u/"
+REDDIT_PASSWORD="MyCoolPassword"
+REDDIT_USER_AGENT="script by /u/" # This can be anything you want.
