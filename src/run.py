f3bdecd2a2ce15104fc6424f3f95892be03605e5
MrPowerScripts <playingwithpowershell@gmail.com>
2020-06-13 23:52:40 +0100

    test

--- src/run.py
+++ src/run.py
@@ -4,9 +4,9 @@ from multiprocessing import Process
 
 if __name__ == "__main__":
 
-  p1 = Process(target=web.webapp)
+  # p1 = Process(target=web.webapp)
   p2 = Process(target=init.init)
-  p1.start()
+  # p1.start()
   p2.start()
-  p1.join()
+  # p1.join()
   p2.join()
