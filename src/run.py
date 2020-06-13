87b1e0be1c8e5835487ccb709a6b95b3d58a4864
MrPowerScripts <playingwithpowershell@gmail.com>
2020-06-14 00:01:11 +0100

    pls heroku

--- src/run.py
+++ src/run.py
@@ -4,9 +4,6 @@ from multiprocessing import Process
 
 if __name__ == "__main__":
 
-  # p1 = Process(target=web.webapp)
-  p2 = Process(target=init.init)
-  # p1.start()
-  p2.start()
-  # p1.join()
-  p2.join()
+  p1 = Process(target=web.webapp)
+  p1.start()
+  init.init()
