71c40e4878d24a68d9da327d9be337aa2a129f10
MrPowerScripts <playingwithpowershell@gmail.com>
2020-06-14 09:25:18 +0100

    this is gonna need to be a module

--- src/run.py
+++ src/run.py
@@ -1,9 +1,9 @@
 import init
-import web
+from web import webapp
 from multiprocessing import Process
 
 if __name__ == "__main__":
 
-  p1 = Process(target=web.webapp)
+  p1 = Process(target=webapp.run)
   p1.start()
   init.init()
