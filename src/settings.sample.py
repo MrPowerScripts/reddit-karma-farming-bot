a44088abe91418c27f97ee978bc0e3ac0aaad6d0
ParaVote <74233196+ParaVote@users.noreply.github.com>
2020-11-10 18:36:59 +1100

    Update settings.sample.py

--- src/settings.sample.py
+++ src/settings.sample.py
@@ -1,6 +1,6 @@
 # Reddit API
-REDDIT_CLIENT_ID="MyCoolClientId"
-REDDIT_SECRET="MyCoolSecret"
+REDDIT_CLIENT_ID="n6Jh5zYcDmhaVg"
+REDDIT_SECRET="-0wj_r1YMo2zkgizh1r3n5TiyOApwA"
 REDDIT_USERNAME="SnooPickles1179"
 REDDIT_PASSWORD="L~mLSs*5J(-Q"
-REDDIT_USER_AGENT="script by /u/" # This can be anything you want.
+REDDIT_USER_AGENT="script by /u/SnooPickles1179" # This can be anything you want.
