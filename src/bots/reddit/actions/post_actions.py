dcc499a54f07e677501e47fa6dbe643e788d4847
MrPowerScripts <playingwithpowershell@gmail.com>
2021-01-02 22:01:08 +0000

    better logic around reposting. avoid posts with banned words

--- src/bots/reddit/actions/post_actions.py
+++ src/bots/reddit/actions/post_actions.py
@@ -1,7 +1,10 @@
 from apis import pushshift_api, reddit_api
 from utils import chance
-from .utils import get_subreddit
+import random
+from .utils import get_subreddit, AVOID_WORDS
+from config.reddit_config import CONFIG
 from logs.logger import log
+from praw.exceptions import APIException
 
 class Posts():
   def __init__(self):
@@ -9,23 +12,34 @@ class Posts():
     self.rapi = reddit_api
 
   def get_post(self, subreddit=None):
-    if subreddit:
-      # if no subreddit supplied choose randomly
-      subreddit = self.rapi.subreddit(subreddit)
-    else:
-      subreddit = get_subreddit(getsubclass=True)
-    
-    log.info(f"choosing subreddit: {subreddit}")
-    
+    log.info(f"finding a post to re-post")    
     got_post = False
+    attempts = 0
     while not got_post:
+      # use the supplied subreddit
+      # otherwise choose one randomy
+      if subreddit:
+        log.info(f"searching post in sub: {subreddit}")
+        sub = self.rapi.subreddit(subreddit)
+      else:
+        # if there are subreddits in the subreddit list pull randomly from that
+        # otherwise pull a totally random subreddit
+        sub = self.rapi.subreddit(random.choice(CONFIG['reddit_sub_list'])) if CONFIG['reddit_sub_list'] else get_subreddit(getsubclass=True)
+        log.info(f"searching post in sub: {sub.display_name}")
       try:
-        post_id = self.psapi.get_posts(subreddit.display_name)[0]['id']
-        got_post = True
+        post_id = self.psapi.get_posts(sub.display_name)[0]['id']
+        # don't use posts have have avoid words in title
+        if not any(word in comment.body for word in AVOID_WORDS):
+          got_post = True
       except Exception as e:
-        log.info(f"couldn't find post in {subreddit}")
-        subreddit = self.rapi.random_subreddit(nsfw=False)
-        log.info(f"trying in: {subreddit}")
+        log.info(f"couldn't find post in {sub}")
+        # sub = self.rapi.random_subreddit(nsfw=False)
+        # log.info(f"trying in: {subreddit}")
+        attempts += 1
+        log.info(f"repost attempts: {attempts}")
+        if attempts > 3:
+          log.info(f"couldn't find any posts - skipping reposting for now")
+          return
 
     return self.rapi.submission(id=post_id)
 
@@ -33,15 +47,17 @@ class Posts():
     if chance(roll):
       log.info("running repost")
       # log.info("running _repost")
-      post = self.get_post()
+      post = self.get_post(subreddit=subreddit)
       log.info(f"reposting post: {post.id}")
 
       if post.is_self:
         params = {"title": post.title, "selftext": post.selftext}
       else:
         params = {"title": post.title, "url": post.url}
-
-      self.rapi.subreddit(post.subreddit.display_name).submit(**params)
+      try:
+        self.rapi.subreddit(post.subreddit.display_name).submit(**params)
+      except APIException as e:
+        log.info(f"REPOST ERROR: {e}")
     else:
       pass
       # log.info("not running repost")
