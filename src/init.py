37538908581a5ed9dc4779fe3e28ce5c47832c2f
Samuel <samuelcarballogarcia8@gmail.com>
2021-01-03 14:19:09 +0100

    Add internet checks

--- src/init.py
+++ src/init.py
@@ -1,9 +1,16 @@
 import sys
+from logs.logger import log
+from utils import check_internet
 import bot
 
 if __name__ == "__main__":
-  try:
-    bot.run()
-  except KeyboardInterrupt:
-    # quit
-    sys.exit()
+    if check_internet() is True:
+        try:
+            log.info('Internet connection found')
+            bot.run()
+        except KeyboardInterrupt:
+            # quit
+            sys.exit()
+        else:
+            log.info('Please check your internet connection')
+            sys.exit()
