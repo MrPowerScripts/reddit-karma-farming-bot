0017d0140ca5751bccbcc48ab096181a6f4eb9d7
igosad <65003317+igosad@users.noreply.github.com>
2021-01-05 13:30:59 +0100

    Update utils.py

--- src/bots/reddit/utils.py
+++ src/bots/reddit/utils.py
@@ -11,7 +11,7 @@ def parse_user(user: Redditors):
   i['comment_karma'] = user.comment_karma
   i['link_karma'] = user.link_karma
   i['username'] = user.name
-  i['created_utc'] = user.created_utc
+  i['created_utc'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(user.created_utc)))
 
   return i
 
