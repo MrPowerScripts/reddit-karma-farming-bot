6bbf02bb6f13de85a3a74b3a5dd549dffbbcb043
MrPowerScripts <playingwithpowershell@gmail.com>
2021-01-09 11:48:35 +0000

    improve windows docs and handling

--- deps/windows/windows.ps1
+++ deps/windows/windows.ps1
@@ -1,2 +1,10 @@
-Invoke-WebRequest -Uri https://download.lfd.uci.edu/pythonlibs/z4tqcw5k/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl -OutFile ./deps/windows/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl
+if (Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools") {
+  Write-Host "Found VS 2019 Build Tools - Excellent"
+} else {
+  Write-Host "VS 2019 Build Tools not found"
+  Write-Host "Read windows installation guide https://github.com/MrPowerScripts/reddit-karma-farming-bot/blob/master/docs/3-windows.md"
+  exit 1
+}
+
+# Invoke-WebRequest -Uri https://download.lfd.uci.edu/pythonlibs/z4tqcw5k/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl -OutFile ./deps/windows/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl
 
--- docs/3-windows.md
+++ docs/3-windows.md
@@ -1,3 +1,9 @@
-# Running the bot on Windows
+# Running the bot on Windows 10
+
+## Install Visual Studio 2019 Build Tools
+
+Download the Visual Studio 2019 Build tools [from this link](https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=16). Then install the C++ build tools workload. You can disable all optional packages except for the windows 10 SDK
+
+![image](https://user-images.githubusercontent.com/1307942/104090619-da2a8800-526f-11eb-858e-ecae03d14a29.png)
 
 Run `run_windows.bat`
