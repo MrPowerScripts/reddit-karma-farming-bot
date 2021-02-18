b8d391329259ced2bdbe437c8d11ac1e829d01f5
MrPowerScripts <playingwithpowershell@gmail.com>
2021-02-18 14:45:06 +0000

    fix dependencies not being installed

--- deps/windows/windows.ps1
+++ deps/windows/windows.ps1
@@ -21,3 +21,18 @@ if (Test-Path -Path "./deps/windows/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl") {
 } else {
   Invoke-WebRequest -Uri https://download.lfd.uci.edu/pythonlibs/z4tqcw5k/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl -OutFile ./deps/windows/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl
 }
+
+#check if pipenv is installed
+if (!(get-command pipenv)) {
+  write-host "Pipenv not found - installing"
+  & pip3 install pipenv
+  exit 1
+} else {write-host "Pipenv found!"}
+
+#check for pipenv dependencies
+if (Test-Path -Path "./.venv") {
+  Write-Host "Pipenv deps installed"
+} else {
+  & pip3 install ./deps/windows/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl
+  & pipenv install
+}
