a37ede843007f60005763e27454f9319db9a94d9
MrPowerScripts <playingwithpowershell@gmail.com>
2021-02-18 14:46:33 +0000

    handle dependencies in powershell

--- run_windows.bat
+++ run_windows.bat
@@ -3,14 +3,6 @@
 Powershell.exe -executionpolicy bypass -File ./deps/windows/windows.ps1
 if errorlevel 1 pause & exit 
 
-if not exist ".env-created" (
-  echo. 2>.env-created
-  echo no virtualenv detected doing setup before running
-  pip3 install pipenv
-  pip3 install ./deps/windows/PyStemmer-2.0.1-cp39-cp39-win_amd64.whl
-  pipenv install
-)
-
 if "%1"=="" (
   echo running without menu
   pipenv run python ./src/init.py
