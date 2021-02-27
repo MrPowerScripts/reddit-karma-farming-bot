4c241e314f9c83516cbce37c6740a7b5adf403ee
igosad <65003317+igosad@users.noreply.github.com>
2021-02-27 19:02:54 +0100

    check if images are deleted
    i think i messed up the indentations

--- src/bots/reddit/actions/post_actions.py
+++ src/bots/reddit/actions/post_actions.py
@@ -56,23 +56,30 @@ class Posts():
       log.info("running repost")
       # log.info("running _repost")
       post = self.get_post(subreddit=subreddit)
-      log.info(f"reposting post: {post.id}")
+      p_url = post.url
+      while p_url.endswith(('.jpg', '.png', '.gif', '.jpeg')):
+        if requests.get(p_url).status_code == 404 :
+          break
+        else : 
+          log.info(f"reposting post: {post.id}")
 
-      if post.is_self:
-        params = {"title": post.title, "selftext": post.selftext}
-      else:
-        params = {"title": post.title, "url": post.url}
+          if post.is_self:
+            params = {"title": post.title, "selftext": post.selftext}
+          else:
+            params = {"title": post.title, "url": post.url}
 
-      sub = post.subreddit
+          sub = post.subreddit
 
-      # randomly choose a potential subreddit to cross post
-      if CONFIG['reddit_crosspost_enabled']:
-        sub = self.rapi.subreddit(self.crosspost(sub.display_name))
+          # randomly choose a potential subreddit to cross post
+          if CONFIG['reddit_crosspost_enabled']:
+            sub = self.rapi.subreddit(self.crosspost(sub.display_name))
 
-      try:
-        self.rapi.subreddit(sub.display_name).submit(**params)
-      except APIException as e:
-        log.info(f"REPOST ERROR: {e}")
+          try:
+            self.rapi.subreddit(sub.display_name).submit(**params)
+            break
+          except APIException as e:
+            log.info(f"REPOST ERROR: {e}")
+            break
     else:
       pass
       # log.info("not running repost")
